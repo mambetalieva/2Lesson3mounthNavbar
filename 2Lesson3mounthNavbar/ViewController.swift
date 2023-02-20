7a79dd24464e479e2bbf6a32ccc5f636e8650b8f
kayra <mgulkajyr@inbox.ru>
2023-02-20 13:27:38 +0600

    Initial Commit

---
+++ 2Lesson3mounthNavbar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2Lesson3mounthNavbar
+//
+//  Created by Каира on 20.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
